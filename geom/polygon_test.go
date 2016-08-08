19ad687a58751814f904e45c326ca70e74498c4e
Uditha Atukorala <ua@nuked.zone>
2016-08-08 23:44:03 +0100

    cleanup

--- geom/polygon_test.go
+++ geom/polygon_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 
-func TestMbr( t *testing.T ) {
+func TestPolygonMbr( t *testing.T ) {
 	input := []Polygon{
 		Polygon{ []*Point{ &Point{ 1, 1 }, &Point{ 2, 3 }, &Point{ 3, 2 } } },
 		Polygon{ []*Point{ &Point{ -1.05, 0.25 }, &Point{ -2, 0.02 }, &Point{ 0.65, 1.78 }, &Point{ 1.25, -1.56 } } },
@@ -31,7 +31,7 @@ func TestMbr( t *testing.T ) {
 	}
 }
 
-func BenchmarkMbr( b *testing.B ) {
+func BenchmarkPolygonMbr( b *testing.B ) {
 	b.ReportAllocs()
 	poly, _ := NewPolygon( []*Point{ &Point{ -1.05, 0.25 }, &Point{ -2, 0.02 }, &Point{ 0.65, 1.78 }, &Point{ 1.25, -1.56 } } )
 	for i := 0; i < b.N; i++ {
